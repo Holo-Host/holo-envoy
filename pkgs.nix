ec667c050574c6098e3537c60dd7f8b34bfc70e9
Yegor Timoshenko <yegortimoshenko@riseup.net>
2020-03-13 15:29:49 +0300

    Bump Holo Nixpkgs pin to b6137d8abb302666128943e37a7ee893bf0f4b0b

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/4d9bbf9971faac5e7fe73cb2189e4f0ee94778c8.tar.gz";
-  sha256 = "1fbzq4dfm03bhnz76smmklkydqb0z8wxsy7rlsm8y6pd6c53dzsq";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/b6137d8abb302666128943e37a7ee893bf0f4b0b.tar.gz";
+  sha256 = "1si369a25fa9kxx0v50yv7p4g6dm1c6dm21fpyzd1fhplhl9m9pz";
 })
