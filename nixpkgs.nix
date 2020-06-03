8e0c1a9c5b40bf852120b171d97ec7d123e1b19e
Oleksii Filonenko <brightone@protonmail.com>
2020-06-03 18:36:34 +0300

    treewide: remove nix files
    Packaging of envoy for nix is handled by holo-nixpkgs.

--- nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/b6137d8abb302666128943e37a7ee893bf0f4b0b.tar.gz";
-  sha256 = "1si369a25fa9kxx0v50yv7p4g6dm1c6dm21fpyzd1fhplhl9m9pz";
-})
