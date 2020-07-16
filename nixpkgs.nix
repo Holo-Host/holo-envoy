8c217fefd8349c08d6c857b1c6dabd68e446a528
Eric Harris-Braun <eric@harris-braun.com>
2020-07-16 11:47:29 -0500

    update nixpkgs for holochain v0.0.50-alpha4

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/82c05d41d972f2674d687d0e6c5c144ff1a6c4e5.tar.gz";
-  sha256 = "1vgafm37dazvrp00i62bqdsr3dj3ybcdb7l92byngizh2zd7pi7r";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/61d4ab61ed9f7e1f93484deb0b0c19a5bec414e4.tar.gz";
+  sha256 = "0cfhnpp797yv4awdd3iqmq8pmv4skx9mbgns7k0byz82v1vxncqk";
 })
