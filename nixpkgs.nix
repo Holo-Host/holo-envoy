8d768121c0988c9ed9a72edb1d1d7b093130b04a
Matthew Brisebois <matthew@webheroes.ca>
2020-07-21 13:04:22 -0600

    Fix nixpkgs revision for holochain v0.0.50-alpha4

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/f80136902a722dd437f7ecc66e3c0fde908c9047.tar.gz";
-  sha256 = "01pnb408ax01w0v51g0c13mz2jk3lslf06byciaimkg4cdnclgrc";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/ef69001d0748ac9ab7f3686be85840028807f349.tar.gz";
+  sha256 = "14hfzsvj3ag9mj0l0gvdg9lgwdvpymvqbicbik1hjnv9vw62380i";
 })
