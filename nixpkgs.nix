bd0885379d4c44cf2be5b0b92ecae5fd08ee696c
Matthew Brisebois <matthew@webheroes.ca>
2020-08-07 09:28:27 -0600

    Bump holochain-rust to d1ade61b9085cc49666d2211f891d2607436ab52 with fixed signing panic

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/ef69001d0748ac9ab7f3686be85840028807f349.tar.gz";
-  sha256 = "14hfzsvj3ag9mj0l0gvdg9lgwdvpymvqbicbik1hjnv9vw62380i";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/d1ade61b9085cc49666d2211f891d2607436ab52.tar.gz";
+  sha256 = "1a0c331cahpyi1xsjp7sy8s9sa3vf6ab1wdmdz44a7rwmzm9nl9p";
 })
