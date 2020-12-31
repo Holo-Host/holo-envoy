2ffda96881287c7b2408d7224b02d2031e636050
jetttech <lisa.jetton@holo.host>
2020-12-31 12:36:26 -0600

    setup dev env

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/8b51eda9384d55a1ced27e7326dfa0e6125dc02c.tar.gz";
-  sha256 = "0gzyxafvs4wkn0hx8v01bwk3najz8fqf9drqlvmx91k2lrzj8xxc";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/3c32fedf012e3bfd27f54dfdb4258a45747c08f3.tar.gz";
+  sha256 = "1nkazknjwncc207msbwfas4q4gn373r8np726xqjfl9867z1ic9c";
 })
