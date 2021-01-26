98d405f8e12dfeeffe7129b22c0365abe279c6e3
Joel <joelulahanna@gmail.com>
2021-01-26 17:05:04 -0600

    update nix-shell

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/3c32fedf012e3bfd27f54dfdb4258a45747c08f3.tar.gz";
-  sha256 = "1nkazknjwncc207msbwfas4q4gn373r8np726xqjfl9867z1ic9c";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/2dd8e2108324f5cb08cc94f857278865ce2525b0.tar.gz";
+  sha256 = "0alqgay5hdlk267615sjlfv6q8v745c18hfzwa0yny74r5w42s9c";
 })
