cc4fbce0b770159af031e82e494592e79d82126b
jetttech <lisa.jetton@holo.host>
2021-01-26 18:59:40 -0600

    update dna urls

--- scripts/fetch-dna.sh
+++ scripts/fetch-dna.sh
@@ -1,4 +1,4 @@
 mkdir dnas
-curl https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha3/holo-hosting-app.dna.gz -o ./dnas/holo-hosting-app.dna.gz
-curl https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.0.1-alpha3/servicelogger.dna.gz -o ./dnas/servicelogger.dna.gz
-curl -LJ https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha9/elemental-chat.dna.gz -o ./dnas/elemental-chat.dna.gz
+curl https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha7/holo-hosting-app.dna.gz -o ./dnas/holo-hosting-app.dna.gz
+curl https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.0.1-alpha5/servicelogger.dna.gz -o ./dnas/servicelogger.dna.gz
+curl -LJ https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha17/elemental-chat.dna.gz -o ./dnas/elemental-chat.dna.gz
