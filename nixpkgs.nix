4564519248890cdba87125c5c59f1420991dab41
Joel <joelulahanna@gmail.com>
2021-02-11 14:40:27 -0600

    revert version

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/46ed2c49af034252cc8879e4e33186743fc269c3.tar.gz";
-  sha256 = "1f8xw1fyvdzlss34wbpjv29jsf5wwcbr7sjj5jvl34ql5w3x77pq";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/8c2c80d89620c724812c70a2fc9ec9f912bceda7.tar.gz";
+  sha256 = "0yai05ql8x43ng0dk2q7jp362c9k999w6avc2xarbbkhrgic39zw";
 })
