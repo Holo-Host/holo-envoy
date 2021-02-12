96999fa182e26d756b05d42ec97a37dcd6daf5df
timotree3 <timorcb@gmail.com>
2021-02-11 19:45:29 -0500

    Attempt to fix bug with shim/socket wrong permissions

--- build/wormhole.js
+++ build/wormhole.js
@@ -1,72 +1,83 @@
-const path = require('path');
+const path = require('path')
 const log = require('@whi/stdlog')(path.basename(__filename), {
-  level: process.env.LOG_LEVEL || 'fatal',
-});
+  level: process.env.LOG_LEVEL || 'fatal'
+})
 
-const net = require('net');
+const net = require('net')
 
-const {
-  structs,
-  MessageParser
-} = require('@holochain/lair-client');
-const { Codec } = require('@holo-host/cryptolib');
+const { structs, MessageParser } = require('@holochain/lair-client')
+const { Codec } = require('@holo-host/cryptolib')
 
-async function init(lair_socket, shim_socket, signing_handler) {
-  log.normal('init wormhole');
-  let connections = [];
+async function init (lair_socket, shim_socket, signing_handler) {
+  log.normal('init wormhole')
+  let connections = []
 
-  const shim = net.createServer(async function(conductor_stream) {
-    log.info("New conductor connections");
-    const lair_stream = net.createConnection(lair_socket);
-    const parser = new MessageParser();
+  const shim = net.createServer(async function (conductor_stream) {
+    log.info('New conductor connections')
+    const lair_stream = net.createConnection(lair_socket)
+    const parser = new MessageParser()
 
     connections.push({
-      "lair": lair_stream,
-      "conductor": conductor_stream,
-      "parser": parser,
-    });
+      lair: lair_stream,
+      conductor: conductor_stream,
+      parser: parser
+    })
 
-    lair_stream.pipe(conductor_stream);
-    conductor_stream.pipe(parser);
+    lair_stream.pipe(conductor_stream)
+    conductor_stream.pipe(parser)
 
     for await (let header of parser) {
-      if (header === null)
-        continue;
+      if (header === null) continue
 
-      if (header.wire_type_id === structs.Ed25519.SignByPublicKey.Request.WIRE_TYPE) {
-        log.normal("Intercepted sign by public key");
-        const request = header.wire_type_class.from(await header.payload());
-        const pubkey = request.get(0);
-        const message = request.get(1);
-        const signature = await signing_handler(pubkey, message);
+      if (
+        header.wire_type_id ===
+        structs.Ed25519.SignByPublicKey.Request.WIRE_TYPE
+      ) {
+        log.normal('Intercepted sign by public key')
+        const request = header.wire_type_class.from(await header.payload())
+        const pubkey = request.get(0)
+        const message = request.get(1)
+        const signature = await signing_handler(pubkey, message)
 
         if (signature !== null) {
-          let response = new structs.Ed25519.SignByPublicKey.Response(Codec.Signature.decode(signature));
-          conductor_stream.write(response.toMessage(header.id));
-          continue;
+          let response = new structs.Ed25519.SignByPublicKey.Response(
+            Codec.Signature.decode(signature)
+          )
+          conductor_stream.write(response.toMessage(header.id))
+          continue
         }
       }
 
-      log.normal("Forwarding message to Lair");
-      header.forward(lair_stream);
+      log.normal('Forwarding message to Lair')
+      header.forward(lair_stream)
     }
-  });
-  shim.listen(shim_socket);
+  })
+  // Make sure that the socket is accessible to holochain (needs read+write access to connect)
+  const prevMask = process.umask(0o000) // 000 on a file results in rw-rw-rw-
+  shim.listen(shim_socket)
+  // Reset umask and check if it changed since we last set it
+  const prevMask2 = process.umask(prevMask)
+  if (prevMask2 !== 0o000) {
+    log.warn(
+      `umask changed unexpectedly during creating of lair shim. Unexpected umask: ${prevMask2.toString(
+        8
+      )}`
+    )
+  }
 
   return {
-    stop() {
-      log.normal("Stopping wormhole");
+    stop () {
+      log.normal('Stopping wormhole')
       connections.map(conns => {
-        conns.lair.destroy();
-        conns.conductor.destroy();
-        conns.parser.stop();
-      });
-      return new Promise(f => shim.close(f));
-    },
-  };
+        conns.lair.destroy()
+        conns.conductor.destroy()
+        conns.parser.stop()
+      })
+      return new Promise(f => shim.close(f))
+    }
+  }
 }
 
-
 module.exports = {
-  init,
-};
+  init
+}
