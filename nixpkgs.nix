400619a394ecf82c013891d603b9a25f76c407d3
zo-el <joelulahanna@gmail.com>
2021-03-23 13:42:46 -0500

    update holochain

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/64ebb317ea7cbb3d1aa7801f4f1e9766e1c6165d.tar.gz";
-  sha256 = "1f1iw6xi7bnwkiybl3rbva8pxsrjc63cwh516sqgrlhwn03wdnjm";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/c37d81c5bbf67830f7e8a9e165109f80a36184f3.tar.gz";
+  sha256 = "17rd9v1svslz80csn49vnxrvjns13h9x3s8ml734sdwa5fclh8s5";
 })
