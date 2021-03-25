1c564560d8a8c1d36d7472f05d2a1fb9db31a9ba
Joel U <joelulahanna@gmail.com>
2021-03-25 13:40:33 -0500

    Update script/install-bundles/src/lib.rs
    Co-authored-by: Timo <timorcb@gmail.com>

--- script/install-bundles/src/lib.rs
+++ script/install-bundles/src/lib.rs
@@ -35,7 +35,6 @@ async fn main() -> anyhow::Result<()> {
     let config = ConductorConfig::load_yaml(Path::new("./config.yaml"))?;
     println!("Generating sandbox..");
     let path = hc_sandbox::generate::generate_with_config(Some(config), Some(hc_dir.clone()), Some(PathBuf::from(".sandbox")))?;
-    // let path = hc_sandbox::generate::generate(Some(network.clone()), Some(PathBuf::from(r"../../")), Some(PathBuf::from(r".sandbox")))?;
     println!("Saving in .hc s..");
     hc_sandbox::save::save(hc_dir, vec![path.clone()])?;
 
