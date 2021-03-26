f2fe71e5b60232beb11902ee785a63b08d471fcd
zo-el <joelulahanna@gmail.com>
2021-03-26 03:40:56 -0500

    update sha

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
   url = "https://github.com/Holo-Host/holo-nixpkgs/archive/d5a7ad236255680dc7426b14d20a3a7b9b4d26f3.tar.gz";
-  sha256 = "17rd9v1svslz80csn49vnxrvjns13h9x3s8ml734sdwa5fclh8sa";
+  sha256 = "124jv5v1rblf13ma3fyrxjg85fz502l50gl0jgkn5d0bsnyxwbxa";
 })
