5c5f7e368689de9dcb89a3e0ddf42d482363fe2b
jetttech <ldjetton21@gmail.com>
2021-04-26 03:00:46 -0500

    bump holo-nixpkgs rev with hc bump to rev 4a17800

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/c43f4e478e083cd14eb30991fab5edb22fc6dfef.tar.gz";
-  sha256 = "1vs9s879i9l8hy44wyr8869cflhah81rb31sgccag01wnzr6vdkv";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/89f620a29fb4f88ab0b62faf35e54ce4a79ad8cd.tar.gz";
+  sha256 = "1inmr2gi1njzddb6qxmplazkya5b13hblags3aqikmc56v78alv4";
 })
