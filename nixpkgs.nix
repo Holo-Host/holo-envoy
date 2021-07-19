a21395e1021be5b55c61cb16216e8f3133e3b6fa
jetttech <ldjetton21@gmail.com>
2021-07-19 03:08:02 -0500

    update nixpkgs and hc versions; update dnas to compatible dna versions

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/fb8f355ad8bb79e1eb15c2b37f81a041cec58338.tar.gz";
-  sha256 = "1fs1p6x05vgcq3fxwkz1y7hyb2c7321cmrsdqjwbr6jrikvig8mq";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/ff5698bcfaad1212fc327e778b4c87a1bd7ae1b0.tar.gz";
+  sha256 = "1q45whbrj804gb296b0lb6gr0m242in7d9z7y549amc26z1crwb9";
 })
