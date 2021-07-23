1bed8fd90a216b87c06c79d1ddd4ff5b7431ab12
jetttech <ldjetton21@gmail.com>
2021-07-23 17:57:37 -0500

    restore ec-sl, update ec-happ-id

--- script/install-bundles/src/lib.rs
+++ script/install-bundles/src/lib.rs
@@ -70,22 +70,28 @@ async fn main() -> anyhow::Result<()> {
     let agent_key = hc_sandbox::calls::generate_agent_pub_key(&mut cmd).await?;
 
     // Choose an app id and properties.
-    let ec_id = "ec-happ-id".to_string();
+    // Hosted App (with ec dna) with happ_id: uhCkkCQHxC8aG3v3qwD_5Velo1IHE1RdxEr9-tuNSK15u73m1LPOo
+    let ec_id = "uhCkklzn8qJaPj2t-sbQmGLdEMaaRHtr_cCqWsmP6nlboU4dD".to_string();
     let ec_happ = PathBuf::from("../../dnas/elemental-chat.happ");
-    // Hosted App with happ_id: uhCkkCQHxC8aG3v3qwD_5Velo1IHE1RdxEr9-tuNSK15u73m1LPOo
+
+    // Hosted App (with dummy dna) with happ_id: uhCkkCQHxC8aG3v3qwD_5Velo1IHE1RdxEr9-tuNSK15u73m1LPOo
     let test_id = "uhCkkCQHxC8aG3v3qwD_5Velo1IHE1RdxEr9-tuNSK15u73m1LPOo".to_string();
     let test_happ = PathBuf::from("../../dnas/test.happ");
+
     // NB: Make sure the hha app_name matches the harded HHA_INSTALLED_APP_ID value in index.ts
     let hha_id = "holo-hosting-happ".to_string();
     let hha_happ = PathBuf::from("../../dnas/holo-hosting-app.happ");
 
+    let ec_sl_id = "uhCkklzn8qJaPj2t-sbQmGLdEMaaRHtr_cCqWsmP6nlboU4dD::servicelogger".to_string();
+    let ec_sl_happ = PathBuf::from("../../dnas/servicelogger.happ");
+
     let test_sl_id = "uhCkkCQHxC8aG3v3qwD_5Velo1IHE1RdxEr9-tuNSK15u73m1LPOo::servicelogger".to_string();
     let test_sl_happ = PathBuf::from("../../dnas/servicelogger.happ");
 
-    let ids = [ec_id, test_id, hha_id, test_sl_id];  
-    let happs = [ec_happ, test_happ, hha_happ, test_sl_happ];
-    // Insatalling test happ
-     for i in 0..4_usize {
+    let ids = [ec_id, test_id, hha_id, ec_sl_id, test_sl_id];  
+    let happs = [ec_happ, test_happ, hha_happ, ec_sl_happ, test_sl_happ];
+    // Installing test happ
+     for i in 0..5_usize {
          println!(" Installing {} ", ids[i]);
 
         let a = ProofPayload{
