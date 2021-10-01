226634421b0e6711979b9e42f85166e3557433e8
timotree3 <timorcb@gmail.com>
2021-10-01 15:50:50 -0400

    Update nix setup

--- default.nix
+++ default.nix
@@ -1,41 +1,30 @@
-{ pkgs ? import ./nixpkgs.nix {} }:
-
-with pkgs;
-
-{
-  holo-envoy = mkYarnPackage rec {
-    name = "holo-envoy";
-    src = gitignoreSource ./.;
-
-    nativeBuildInputs = [
-      nodejs
-      makeWrapper
-      ps
-      openssl
-    ];
-
-    packageJSON = "${src}/package.json";
-    yarnLock = "${src}/yarn.lock";
-
-    buildPhase = ''
-      yarn build
-    '';
-
-    installPhase = ''
-        mkdir $out
-        mv node_modules $out
-        cd deps/@holo-host/envoy/
-        mv build server.js $out
-        makeWrapper ${nodejs}/bin/node $out/bin/${name} \
-          --add-flags $out/server.js
-    '';
-
-    fixupPhase = ''
-      patchShebangs $out
-    '';
-
-    distPhase = '':'';
-
-    doCheck = true;
+let
+  holonixPath = builtins.fetchTarball {
+    url = "https://github.com/holochain/holonix/archive/55a5eef58979fb6bc476d8c3e0c028cdeb1b5421.tar.gz";
+    sha256 = "sha256:0q6d0rql1pyy93xw1c8s28jjjcgk1zgwxwixsp9z5r4w2ihaz3zg";
   };
+  holonix = import (holonixPath) {
+    includeHolochainBinaries = true;
+    holochainVersionId = "custom";
+
+    holochainVersion = {
+     rev = "a1206a694fe3b521440fe633db99a50b8255c1b2";
+     sha256 = "0qdjjkqw3xlg8g686gvn509a9rv4kc6qfw07hypzc0fksix9d4iz";
+     cargoSha256 = "sha256:175b76j31sls0gj08imchwnk7n4ylsxlc1bm58zrhfmq62hcchb1";
+     bins = {
+       holochain = "holochain";
+       hc = "hc";
+       kitsune-p2p-proxy = "kitsune_p2p/proxy";
+     };
+     lairKeystoreHashes = {
+        sha256 = "0khg5w5fgdp1sg22vqyzsb2ri7znbxiwl7vr2zx6bwn744wy2cyv";
+        cargoSha256 = "1lm8vrxh7fw7gcir9lq85frfd0rdcca9p7883nikjfbn21ac4sn4";
+      };
+    };
+    holochainOtherDepsNames = ["lair-keystore"];
+  };
+  nixpkgs = holonix.pkgs;
+in nixpkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  buildInputs = with nixpkgs; [];
 }
--- nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/ff5698bcfaad1212fc327e778b4c87a1bd7ae1b0.tar.gz";
-  sha256 = "1q45whbrj804gb296b0lb6gr0m242in7d9z7y549amc26z1crwb9";
-})
